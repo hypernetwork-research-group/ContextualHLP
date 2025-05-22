b47f18e7a9b880f0ce9e018f848dc8ed5aec7016
DarioDeMaio <demaio.dario95@gmail.com>
2025-05-22 19:31:54 +0200

    feat: new dataset

--- datasets.py
+++ datasets.py
@@ -4,13 +4,42 @@ from torch_geometric.data import InMemoryDataset
 from torch_geometric.data.hypergraph_data import HyperGraphData
 from typing import Callable
 
+class SplitDataset(InMemoryDataset):
+
+    def __getitem__(self, idx) -> HyperGraphData:
+        if isinstance(idx, int):
+            idx = torch.tensor([idx])
+        if isinstance(idx, list):
+            idx = torch.tensor(idx)
+        edge_index_mask = torch.isin(self._data.edge_index[1], idx)
+        edge_index = self._data.edge_index[:, edge_index_mask]
+        _, edge_index[1] = edge_index[1].unique(return_inverse=True)
+        return HyperGraphData(
+            x=self._data.x,
+            edge_index=edge_index,
+            edge_attr=self._data.edge_attr[idx],
+        )
+
+    def __len__(self) -> int:
+        return self._data.edge_index[1].max().item() + 1
+
 def train_test_split(dataset: InMemoryDataset, test_size: float = 0.2):
     indices = torch.randperm(len(dataset), device=dataset.x.device)
     split = int(len(dataset) * (1 - test_size))
     train_indices = torch.sort(indices[:split]).values
     test_indices = torch.sort(indices[split:]).values
-    train_dataset = dataset[train_indices]
-    test_dataset = dataset[test_indices]
+    train_data = dataset[train_indices]
+    class TrainDataset(SplitDataset):
+        def __init__(self):
+            super().__init__()
+            self._data = train_data
+    train_dataset = TrainDataset()
+    test_data = dataset[test_indices]
+    class TestDataset(SplitDataset):
+        def __init__(self):
+            super().__init__()
+            self._data = test_data
+    test_dataset = TestDataset()
     train_mask = torch.zeros(len(dataset), dtype=torch.bool, device=dataset.x.device)
     train_mask[train_indices] = True
     test_mask = torch.zeros(len(dataset), dtype=torch.bool, device=dataset.x.device)
@@ -128,7 +157,12 @@ class IMDBHypergraphDataset(CHLPBaseDataset):
 
 class ARXIVHypergraphDataset(CHLPBaseDataset):
     
-    GDRIVE_ID = "1XI4428OfnHlNaGpJN1BFPf5gKSpEEawh"
+    GDRIVE_ID = "1nGkihnayNx4PskOHqiqwUYcYoGF3t-1H"
     DATASET_NAME = "ARXIV"
+
+class COURSERAHypergraphDataset(CHLPBaseDataset):
+    
+    GDRIVE_ID = "1AVqUuVOpCFVG13-N3lUckYhIrk83WOSu"
+    DATASET_NAME = "COURSERA"
 
 torch.serialization.add_safe_globals([HyperGraphData])
