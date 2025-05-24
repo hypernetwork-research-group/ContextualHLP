0706e963b611cbd0211d446710ab4b88750ffc0a
DarioDeMaio <demaio.dario95@gmail.com>
2025-05-24 20:11:59 +0200

    fix: pipe

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import argparse
 from src.training import *
 from src.utils import load_and_prepare_data
 import torch
+import os
 
 def main():
     parser = argparse.ArgumentParser(description="Train a 3-model pipeline")
@@ -15,7 +16,12 @@ def main():
     train_loader, val_loader, test_loader, num_features = load_and_prepare_data(args.dataset, args.mode, args.batch_size, args.num_workers)
     model = create_model(num_features)
     run_training(model, train_loader, val_loader, args.epochs)
-    torch.save(model, f'./model_checkpoints/{args.dataset}_{args.mode}.pth')
+
+    checkpoint_dir = f'./model_checkpoints/{args.dataset}'
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+        
+    torch.save(model, f'{checkpoint_dir}/{args.mode}.pth')
     run_test_and_save_results(
         model,
         test_loader=test_loader,
